5798d454f50cd6cd1f2b371ce1de61483ec1035c
王然 <ranwang@alauda.io>
2023-01-15 21:55:54 +0800

    feat: Clarify the response of API 404

--- httpbin/handlers.go
+++ httpbin/handlers.go
@@ -23,7 +23,8 @@ func notImplementedHandler(w http.ResponseWriter, r *http.Request) {
 // Index renders an HTML index page
 func (h *HTTPBin) Index(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		http.Error(w, "Not Found", http.StatusNotFound)
+		msg := fmt.Sprintf("Not Found (go-httpbin does not have the API: %s)", r.URL.Path)
+		http.Error(w, msg, http.StatusNotFound)
 		return
 	}
 	w.Header().Set("Content-Security-Policy", "default-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self' camo.githubusercontent.com")
